0210a6125ca64bd8981f718f0db4784656257406
SONAL GUPTA <72059990+gsonal899@users.noreply.github.com>
2023-10-18 22:27:33 +0530

    Delete Travel-PlannerProject directory

--- Travel-PlannerProject/src/main/java/com/example/travel/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.travel.exception;
-
-public class UserAlreadyExistsException extends RuntimeException
-{
-	public UserAlreadyExistsException(String resourceName,String fieldName,String fieldValue)
-	{
-		super(String.format("%s already exists with given %s : '%s'", resourceName, fieldName, fieldValue));
-	}
-}
